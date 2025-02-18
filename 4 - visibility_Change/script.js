e56dffdee06b8123b91c961de4fc895e79d217b0
Bens <Jefferson.b3ntz@gmail.com>
2025-02-18 12:31:13 -0300

    visibilityChange

---
+++ 4 - visibility_Change/script.js
@@ -0,0 +1,7 @@
+document.querySelector("#appBtnChat").addEventListener("click", function (){
+    document.querySelector("#notification-box-chat").style.visibility = "visible";
+});
+
+document.querySelector("#closeBtnChat").addEventListener("click", function (){
+    document.querySelector("#notification-box-chat").style.visibility = "hidden";
+});
