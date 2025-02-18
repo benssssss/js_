77e945770b5d99bcdfe68a4374134780a1f101c0
Bens <Jefferson.b3ntz@gmail.com>
2025-02-18 17:07:22 -0300

    dois testes de JS dos proximos projetos do frontend mentor

---
+++ 5 - FAQdetailRevealTest/script.js
@@ -0,0 +1,19 @@
+// ------------- firstLine ----------
+
+document.querySelector("#open-close").addEventListener("click", function () {
+    document.querySelector("#description").classList.toggle("style-none");
+});
+
+document.querySelector("#open-close").addEventListener("click", function () {
+    document.querySelector("#open-close").classList.toggle("another-class");
+});
+
+// ------------- secondLine ----------
+
+document.querySelector("#open-close-1").addEventListener("click", function () {
+    document.querySelector("#description-1").classList.toggle("style-none");
+});
+
+document.querySelector("#open-close-1").addEventListener("click", function () {
+    document.querySelector("#open-close-1").classList.toggle("another-class");
+});
---
+++ 6 - ArticlePreviewTest/script.js
@@ -0,0 +1,3 @@
+document.querySelector("#share-button").addEventListener("click", function () {
+    document.querySelector("#script-box").classList.toggle("script-block");
+});
